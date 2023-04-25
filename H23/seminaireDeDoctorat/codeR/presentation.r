6245e1b8d4a70f4830df4c3d1b7b28dc7b53f29a
Laurence-Olivier M. Foisy <mail@mfoisy.com>
2023-04-24 21:33:16 -0400

    ajout Graphk

--- H23/seminaireDeDoctorat/codeR/presentation.r
+++ H23/seminaireDeDoctorat/codeR/presentation.r
@@ -9,3 +9,10 @@ ggplot(data_ch_us, aes(x = year, y = percentExportsUsToChina)) +
   geom_line(aes(y = percentExportsChinaToUsa), color = "red") +
   geom_line(aes(y = percentImportsChinaFromUsa), color = "blue") +
   geom_line(aes(y = percentImportsUsaFromChina), color = "green")
+
+ggplot(data_ch_us, aes(year, exportsUsaToChina, color = "red")) +
+  geom_line() +
+  geom_line(aes(y = importsUsaFromChina, color = "blue")) +
+  theme_classic()
+ggsave(filename = "exportGraph.png",
+       path = "H23/seminaireDeDoctorat/codeR/graphiquesFaitsEnR")
